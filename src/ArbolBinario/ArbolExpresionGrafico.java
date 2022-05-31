fd5d93e29a951d8657f17639cdb6951841cd18ea
Yellsmy <yltojg@alumno.uspg.edu.gt>
2022-05-30 22:01:23 -0600

    Actualizado

--- src/ArbolBinario/ArbolExpresionGrafico.java
+++ src/ArbolBinario/ArbolExpresionGrafico.java
@@ -31,7 +31,7 @@ public class ArbolExpresionGrafico extends JPanel
     {
           this.miArbol = miArbol;
           this.setBackground(Color.WHITE); // Se le asigna color blanco al panel
-          //guardar valores nulos en cualquier lugar 
+          //Creamos 2 mapa hash. HashMap nos ayudará a contener todos los subárboles y posiciones
           posicionNodos = new HashMap();
           subtreeSizes = new HashMap();
           dirty = true;
@@ -57,12 +57,11 @@ public class ArbolExpresionGrafico extends JPanel
     private void calcularPosiciones() 
     {
          posicionNodos.clear(); //Limpia la posición de los nodos
-         subtreeSizes.clear(); //Limpia el arbol
-         Nodo root = this.miArbol.getRaiz();
-         //Si la raíz no es nula, llama a los métodos para calcular el tamaño del subárbol 
-         //y al método que nos dará la posición de cada nodo
+         subtreeSizes.clear(); //Limpia el subarbol
+         Nodo root = this.miArbol.getRaiz(); //creamos una variable que guardará la raiz del árbol      
          if (root != null) 
-         {
+         {//Si la raíz no es nula, llama a los métodos para calcular el tamaño del subárbol 
+         //y al método que nos dará la posición de cada nodo
              calcularTamañoSubarbol(root);
              calcularPosicion(root, Integer.MAX_VALUE, Integer.MAX_VALUE, 0);
          }
@@ -78,31 +77,36 @@ public class ArbolExpresionGrafico extends JPanel
      */
     //El método retornará un objeto dimensión que es una clase de la librería awt
     private Dimension calcularTamañoSubarbol(Nodo n) 
-    {     //Si el árbol está vacío retornará dimensiones altura y ancho 0
+    {     //Si el árbol está vacío retornará dimensiones altura y ancho 0 porque quiere decir que no
+         //existe un árbol
           if (n == null) 
               return new Dimension(0,0);
           
-          //sino se ejecutará la recursividad creando variables de tipo Dimensión para recorrer el árbol
+          //sino, se ejecutará la recursividad creando variables de tipo Dimensión para recorrer el árbol
           //Del lado izquierdo y del lado derecho y lo guarda en las variables ld y rd respectivamente
           Dimension ld = calcularTamañoSubarbol(n.getIzq());
           Dimension rd = calcularTamañoSubarbol(n.getDer());
           
-          //Creamos variables para guardar 
+          //Creamos variable que sumará los datos:
           //fm.getHeight() nos ayudará a obtener la altura del texto, en este caso, del número
           //parent2child ni idea XD
           //saca la altura máxima del lado izquierdo y derecho que se guardó en ld y rd anteriormente         
           int h = fm.getHeight() + parent2child + Math.max(ld.height, rd.height);
           
-          //El ancho de los datos izquierdo que fue guardado en ld anteriormente
+          //Creamos variable que sumará los datos:
+          //El ancho de los datos izquierdos que fue guardado en ld anteriormente
           //child2child ni idea x2
           //El ancho de los datos derecho que fue guardado en rd anteriormente
           int w = ld.width + child2child + rd.width;
           
-          //inicializamos un nuevo objeto con el ancho y la altura que guardamos en w y h
+          //construye una dimensión que recibe el ancho "w" y la altura "h" que guardamos anteriormente
+          //Para calcular el tamaño de cada subárbol
           Dimension d = new Dimension(w, h);
           
-          //Utilizamos el método put() para insertar una nueva clave y un nuevo valor
+          //Utilizamos el método put() para almacenar un valor "d" y lo asocia con "n" que será la clave de ese valor
           //En este caso la clave es el nodo y el valor es el objeto d
+          //De esta forma es como guarda el tamaño de cada subárbol
+          //Y podrá ser recuperado en cualquier punto dado pasando el nodo que deseemos
           subtreeSizes.put(n, d);
           
           return d;
@@ -121,27 +125,58 @@ public class ArbolExpresionGrafico extends JPanel
      */
     private void calcularPosicion(Nodo n, int left, int right, int top) 
     {
-        
+      //Si no hay datos en el árbol, sale de la función  
       if (n == null) 
           return;
-          
+      
+      //Variable "ld" de tipo dimensión que guarda el valor asociado con la posición
+      //izquierda que se pasa como clave y que fueron almacenadas en el método calcularPosiciones() 
       Dimension ld = (Dimension) subtreeSizes.get(n.getIzq());
+      
+      //Si el valor asociado almacenado en la posición izquierda es nulo,
+      //"ld" guardará las dimensiones 0,0 que fueron iniciadas al inicio
       if (ld == null) 
           ld = empty;
       
+      //Variable "rd" de tipo dimensión que guarda el valor asociado con la posición
+      //derecha que se pasa como clave y que fueron almacenadas en el método calcularPosiciones()
       Dimension rd = (Dimension) subtreeSizes.get(n.getDer());
+      
+      //Si el valor asociado almacenado en la posición derecha es nulo,
+      //"rd" guardará las dimensiones 0,0 que fueron iniciadas al inicio
       if (rd == null) 
           rd = empty;
       
+      //El centro será cero
       int center = 0;
       
+      //si la alineación y orientación a la derecha es diferente al valor máximo almacenado
+      //el centro será el resultado de la resta entre la posición derecha, el ancho del elemento rd y
+      //la división entre 2 del valor almacenado en child2child
       if (right != Integer.MAX_VALUE)
           center = right - rd.width - child2child/2;
+      
+      //si la alineación y orientación a la izquierda es diferente al valor máximo almacenado
+      //el centro será el resultado de la suma entre la posición izquierda, el ancho del elemento ld y
+      //la división entre 2 del valor almacenado en child2child
       else if (left != Integer.MAX_VALUE)
           center = left + ld.width + child2child/2;
+      
+      
+      
+      //Variable que guardará el ancho que tiene el valor almacenado en Dato, en este caso, el ancho del/los
+      //número/s
       int width = fm.stringWidth(n.getDato()+"");
-      //utilizamos el hasmap para hacer referencia a un elemento nulo el cual recibe el n valor nodo de las funciones get creando un rectangulo con ancho alto altura 
+      
+      //Utilizamos el método put() para almacenar un valor asociado a una clave
+      //En este caso la clave será la ubicación del nodo n y el valor será un objeto Rectangle (un área en un espacio de coordenadas)
+      //Que recibe cordenadas (x,y), un ancho "width" y una altura height
+      //x: será el resultado de la resta entre el centro(valor guardado en la variable center) menos la mitad del ancho menos 3
+      //y: será el int con el tope
+      //width: será el resultado de la suma entre el ancho más 6
+      //height: será la altura del texto    
       posicionNodos.put(n,new Rectangle(center - width/2 - 3, top, width + 6, fm.getHeight()));
+      
       //proceso de dibujo recursivo 
       calcularPosicion(n.getIzq(), Integer.MAX_VALUE, center - child2child/2, top + fm.getHeight() + parent2child);
       calcularPosicion(n.getDer(), center + child2child/2, Integer.MAX_VALUE, top + fm.getHeight() + parent2child);
@@ -157,19 +192,24 @@ public class ArbolExpresionGrafico extends JPanel
      * parametro altura: int con la altura del FontMetrics.
      */
     private void dibujarArbol(Graphics2D g, Nodo n, int puntox, int puntoy, int altura) 
-    {
+    {//si la referencia para dibujar el árbol es nula, sale de la función
      if (n == null) 
          return;
      
-     //Creamos una variable de tipo Rectangle que guardará un objeto 
+     //Creamos una variable de tipo Rectangle que guardará el valor asociado a la clave "n" almacenado en el mapa hash
      Rectangle r = (Rectangle) posicionNodos.get(n);
      
-     //Dibujamos 
+     //Dibujamos el exterior del rectángulo donde está el nodo a dibujar en cual fue guardado en "r" 
      g.draw(r);
      
-     //Escribe un string en este caso sería el valor de dato, con las cordenadas específicas
+     //Escribe o "dibuja" un string en este caso sería el valor de dato,
+     //con las cordenadas específicas (string, x, y)
+     //string: el dato del nodo que se va a dibujar
+     //x: resultado de la suma entre la posición en x que fue almacenada en posicionNodos más 3
+     //y: resultado de la suma entre la posición en y que fue almacenada en posicionNodos más la altura del FontMetrics
      g.drawString(n.getDato()+"", r.x + 3, r.y + altura);
    
+     //
      if (puntox != Integer.MAX_VALUE)
        
      g.drawLine(puntox, puntoy, (int)(r.x + r.width/2), r.y);
